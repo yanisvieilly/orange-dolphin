56914773e0069ea4501d65bf4d69d8863076e93b
Yanis Vieilly <yanis.vieilly+github@protonmail.com>
2020-04-07 17:15:52 +0200

    Add anagram check algorithm

--- app.js
+++ app.js
@@ -5,3 +5,36 @@ const words = fs
   .toString()
   .split("\r\n");
 
+const results = [];
+
+words.forEach((sourceWord, i) => {
+  const anagrams = words.slice(i).filter((targetWord) => {
+    if (sourceWord === targetWord || sourceWord.length !== targetWord.length) {
+      return false;
+    }
+
+    for (let i = 0; i < sourceWord.length; i++) {
+      if (!targetWord.includes(sourceWord[i])) {
+        return false;
+      }
+    }
+
+    for (let i = 0; i < targetWord.length; i++) {
+      if (!sourceWord.includes(targetWord[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+  if (!anagrams.length) {
+    return;
+  }
+
+  anagrams.push(sourceWord);
+
+  results.push(anagrams);
+});
+
+console.log(results);
